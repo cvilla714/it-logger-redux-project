25be0723e9f49543e7a3a63e115bc2996220fc79
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-26 15:39:31 -0400

    added the htmlFor to make it work

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -7,7 +7,7 @@ const SearchBar = () => {
         <form>
           <div className="input-field">
             <input id="search" type="search" required />
-            <label className="label-icon" for="search">
+            <label className="label-icon" htmlFor="search">
               <i className="material-icons">search</i>
             </label>
             <i className="material-icons">close</i>
