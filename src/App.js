562244f3c2454e04ae4873dba47bb78df610a9d6
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-27 12:35:08 -0400

    imported Editlogmodal and added component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogsModal';
+import EditLogModal from './components/logs/EditLogModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -19,6 +20,7 @@ const App = () => {
       <div className="container">
         <AddBtn />
         <AddLogModal />
+        <EditLogModal />
         <Logs />
       </div>
     </Fragment>
