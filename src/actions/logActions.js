431d6296033edb74d294aad991489e98b99381a8
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-27 17:50:42 -0400

    added the asyn and await to fetch data

---
+++ src/actions/logActions.js
@@ -0,0 +1,22 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+
+export const getLogs = () => {
+  return async (dispatch) => {
+    setLoading();
+
+    const res = await fetch('/logs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      paylaod: data,
+    });
+  };
+};
+
+// set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
