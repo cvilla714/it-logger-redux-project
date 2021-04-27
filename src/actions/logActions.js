679bd71e367c1d9c6903cb387ef2246d3a56cdf7
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-27 19:14:39 -0400

    corrected misspelled

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -14,6 +14,7 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
 //   };
 // };
 
+//Get logs from server
 export const getLogs = () => async (dispatch) => {
   try {
     setLoading();
@@ -23,7 +24,7 @@ export const getLogs = () => async (dispatch) => {
 
     dispatch({
       type: GET_LOGS,
-      paylaod: data,
+      payload: data,
     });
   } catch (error) {
     dispatch({
