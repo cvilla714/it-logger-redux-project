6c694fbf86370a6f0fe1466a8d249ab32791ef01
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-27 14:04:14 -0400

    imported TechItem and component passed the props as well

--- src/components/techs/TechListModal.js
+++ src/components/techs/TechListModal.js
@@ -23,7 +23,8 @@ const TechListModal = () => {
     <div id="tech-list-modal" className="modal">
       <div className="modal-content">
         <h4>Technician List</h4>
-        <ul className="collection">{!loading && techs.map((tech) => <li className="collection-item">{tech.firstName}</li>)}</ul>
+        {/* <ul className="collection">{!loading && techs.map((tech) => <li className="collection-item">{tech.firstName}</li>)}</ul> */}
+        <ul className="collection">{!loading && techs.map((tech) => <TechItem tech={tech} key={tech.id} />)}</ul>
       </div>
     </div>
   );
