d74aef180268ca380f9a45d345623951144af2f4
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-27 18:07:21 -0400

    added try and catch to handle error fetching data

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,7 +1,21 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
 
-export const getLogs = () => {
-  return async (dispatch) => {
+// export const getLogs = () => {
+//   return async (dispatch) => {
+//     setLoading();
+
+//     const res = await fetch('/logs');
+//     const data = await res.json();
+
+//     dispatch({
+//       type: GET_LOGS,
+//       paylaod: data,
+//     });
+//   };
+// };
+
+export const getLogs = () => async (dispatch) => {
+  try {
     setLoading();
 
     const res = await fetch('/logs');
@@ -11,7 +25,12 @@ export const getLogs = () => {
       type: GET_LOGS,
       paylaod: data,
     });
-  };
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
 };
 
 // set loading to true
