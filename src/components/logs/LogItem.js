cae9030a93be4a825dd1a69692c6f2ba55774a18
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-28 13:26:49 -0400

    imported set_current and added as prop to logitem and also to the connet to have access to it, added the onClick to fire the setCurrent taking the log

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-import { deleteLog } from '../../actions/logActions';
+import { deleteLog, setCurrent } from '../../actions/logActions';
 
 import M from 'materialize-css/dist/js/materialize.min.js';
-const LogItem = ({ log, deleteLog }) => {
+const LogItem = ({ log, deleteLog, setCurrent }) => {
   const onDelete = () => {
     deleteLog(log.id);
     M.toast({ html: 'Log Deleted' });
@@ -13,7 +13,7 @@ const LogItem = ({ log, deleteLog }) => {
   return (
     <li className="collection-item">
       <div>
-        <a href="#edit-log-modal" className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}>
+        <a href="#edit-log-modal" className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`} onClick={() => setCurrent(log)}>
           {log.message}
         </a>
         <br />
@@ -33,6 +33,7 @@ const LogItem = ({ log, deleteLog }) => {
 LogItem.propTypes = {
   log: PropTypes.object.isRequired,
   deleteLog: PropTypes.func.isRequired,
+  setCurrent: PropTypes.func.isRequired,
 };
 
-export default connect(null, { deleteLog })(LogItem);
+export default connect(null, { deleteLog, setCurrent })(LogItem);
