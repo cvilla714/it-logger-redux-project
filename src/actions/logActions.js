b62bbda3505912aa7e63077fb58051b5f0cca040
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-28 02:18:14 -0400

    imported the Delete_logs action from type and created the function to deleted the log with the id

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOGS } from './types';
 
 // export const getLogs = () => {
 //   return async (dispatch) => {
@@ -51,6 +51,28 @@ export const addLog = (log) => async (dispatch) => {
     dispatch({
       type: ADD_LOG,
       payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+
+// Delete log action
+export const deleteLog = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`/logs/${id}`, {
+      method: 'DELETE',
+    });
+    // const data = await res.json();
+
+    dispatch({
+      type: DELETE_LOGS,
+      payload: id,
     });
   } catch (error) {
     dispatch({
