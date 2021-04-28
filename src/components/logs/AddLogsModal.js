22abc958acb862a73f7fc3eb70ce6f48b1c4a0d2
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-28 18:49:05 -0400

    imported the component and added it instead of the hardcoded list

--- src/components/logs/AddLogsModal.js
+++ src/components/logs/AddLogsModal.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import TechSelectOptions from '../techs/TechSelectOptions';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addLog } from '../../actions/logActions';
@@ -49,9 +50,10 @@ const AddLogModal = ({ addLog }) => {
               <option value="" disabled>
                 Select Technician
               </option>
-              <option value="Jon Doe">Jon Doe</option>
+              {/* <option value="Jon Doe">Jon Doe</option>
               <option value="Sam Smith">Sam Smith</option>
-              <option value="Sara Wilson">Sara Wilson</option>
+              <option value="Sara Wilson">Sara Wilson</option> */}
+              <TechSelectOptions />
             </select>
           </div>
         </div>
