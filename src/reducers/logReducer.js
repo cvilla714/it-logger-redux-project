f792dd45a303dba7b4c7d7f8c69b79098d70b987
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-28 13:25:50 -0400

    imported the actions for update_logs,set_current and clear_current, also created each case for it

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOGS } from '../actions//types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOGS, UPDATE_LOG, SET_CURRENT, CLEAR_CURRENT } from '../actions//types';
 const initialState = {
   logs: null,
   current: null,
@@ -26,6 +26,21 @@ export default (state = initialState, action) => {
         ...state,
         logs: state.logs.filter((log) => log.id !== action.payload),
         loading: false,
+      };
+    case UPDATE_LOG:
+      return {
+        ...state,
+        logs: state.logs.map((log) => (log.id === action.paylod.id ? action.payload : log)),
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload,
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
       };
     case SET_LOADING:
       return {
