af0b13ba82e179ffa8d4a2784cc0fd1c1c43bf6c
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-28 14:02:00 -0400

    added connect, proptypes and use effect to set the current message,attention and tech name once the modal is clicked

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import { updateLog } from '../../actions/logActions';
 
-const EditLogsModal = () => {
+const EditLogsModal = ({ current, updateLog }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
+
+  useEffect(() => {
+    if (current) {
+      setMessage(current.message);
+      setAttention(current.attention);
+      setTech(current.tech);
+    }
+  }, [current]);
 
   const onSubmit = () => {
     if (message === '' || tech === '') {
@@ -25,9 +36,9 @@ const EditLogsModal = () => {
         <div className="row">
           <div className="input-field">
             <input type="text" name="message" value={message} onChange={(e) => setMessage(e.target.value)} />
-            <label htmlFor="message" className="active">
+            {/* <label htmlFor="message" className="active">
               Log Message
-            </label>
+            </label> */}
           </div>
         </div>
         <div className="row">
@@ -67,4 +78,13 @@ const modalStyle = {
   height: '75%',
 };
 
-export default EditLogsModal;
+EditLogsModal.propTypes = {
+  current: PropTypes.object,
+  updateLog: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  current: state.log.current,
+});
+
+export default connect(mapStateToProps, { updateLog })(EditLogsModal);
