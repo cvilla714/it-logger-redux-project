88f71e120a8e65fba4b32db4d2a720726578bd8f
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-28 18:09:27 -0400

    imported connect, proptyes and the action to use it as props.

--- src/components/techs/TechListModal.js
+++ src/components/techs/TechListModal.js
@@ -1,33 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import TechItem from './TechItem';
+import { connect } from 'react-redux';
+import { getTechs } from '../../actions/techActions';
+import PropTypes from 'prop-types';
 
-const TechListModal = () => {
-  const [techs, setTechs] = useState([]);
-  const [loading, setLoading] = useState(false);
+const TechListModal = ({ getTechs, tech: { techs, loading } }) => {
+  // const [techs, setTechs] = useState([]);
+  // const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getTechs();
     //elint-disable-next-line
   }, []);
 
-  const getTechs = async () => {
-    setLoading(true);
-    const res = await fetch('/techs');
-    const data = await res.json();
+  // const getTechs = async () => {
+  //   setLoading(true);
+  //   const res = await fetch('/techs');
+  //   const data = await res.json();
 
-    setTechs(data);
-    setLoading(false);
-  };
+  //   setTechs(data);
+  //   setLoading(false);
+  // };
 
   return (
     <div id="tech-list-modal" className="modal">
       <div className="modal-content">
         <h4>Technician List</h4>
         {/* <ul className="collection">{!loading && techs.map((tech) => <li className="collection-item">{tech.firstName}</li>)}</ul> */}
-        <ul className="collection">{!loading && techs.map((tech) => <TechItem tech={tech} key={tech.id} />)}</ul>
+        <ul className="collection">{!loading && techs !== null && techs.map((tech) => <TechItem tech={tech} key={tech.id} />)}</ul>
       </div>
     </div>
   );
 };
 
-export default TechListModal;
+TechListModal.propTypes = {
+  tech: PropTypes.object.isRequired,
+  getTechs: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  tech: state.tech,
+});
+
+export default connect(mapStateToProps, { getTechs })(TechListModal);
