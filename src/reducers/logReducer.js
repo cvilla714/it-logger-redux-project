a12a5f417531e7e37f55c5872adf5c20c5922255
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-28 01:29:35 -0400

    added the case for AddLog

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,5 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from '../actions//types';
+/* eslint-disable import/no-anonymous-default-export */
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from '../actions//types';
 const initialState = {
   logs: null,
   current: null,
@@ -12,6 +13,12 @@ export default (state = initialState, action) => {
       return {
         ...state,
         logs: action.payload,
+        loading: false,
+      };
+    case ADD_LOG:
+      return {
+        ...state,
+        logs: [...state.logs, action.payload],
         loading: false,
       };
     case SET_LOADING:
