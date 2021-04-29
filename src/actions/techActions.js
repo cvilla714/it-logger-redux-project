da3b0c695957c93663e2bebfffbbcd616c83dae0
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-29 01:36:35 -0400

    added the deletetech function

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -46,6 +46,27 @@ export const addTech = (tech) => async (dispatch) => {
   }
 };
 
+//Delete techs from server
+export const deleteTech = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      method: 'DELETE',
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id,
+    });
+  } catch (error) {
+    dispatch({
+      type: TECH_ERROR,
+      payload: error.response.statusText,
+    });
+  }
+};
+
 //set Lodaing to true
 export const setLoading = () => {
   return {
