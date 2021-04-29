40bd13aa7f2eb6d61753a6aba39c6d4403a9375f
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-29 00:26:44 -0400

    added the actions from types and created the cases for add_tech and tech_error

--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { GET_TECHS, ADD_TECH, DELETE_TECH, SET_LOADING, TECHS_ERROR } from '../actions/types';
+import { GET_TECHS, ADD_TECH, DELETE_TECH, SET_LOADING, TECH_ERROR, ADD_LOG } from '../actions/types';
 
 const initialState = {
   techs: null,
@@ -15,10 +15,22 @@ export default (state = initialState, action) => {
         techs: action.payload,
         loading: false,
       };
+    case ADD_TECH:
+      return {
+        ...state,
+        techs: [...state.techs, action.payload],
+        loading: false,
+      };
     case SET_LOADING:
       return {
         ...state,
         loading: true,
+      };
+    case TECH_ERROR:
+      console.error(action.payload);
+      return {
+        error: action.payload,
+        loading: false,
       };
     default:
       return state;
